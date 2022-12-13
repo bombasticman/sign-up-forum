970a8343ca57531afd315df5cd5e791ab10f325b
bombasticman <atakanyaln@gmail.com>
2022-12-13 12:08:22 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+const formFields = document.getElementsByClassName('form-field')
+
+
+function createAccount(formFields){
+    for(i = 0; i < formFields.lenght; i++){
+        if (formFields[i] == )
+    }
+}
